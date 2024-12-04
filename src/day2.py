c669ec052a3df951c3920a64795fb3bad609decc
kevin-wackman <kevin.wackman@gmail.com>
2024-12-03 18:21:01 -0600

    Added part 2 solution to problem 2.
       I thought about memoizing it, but it would require significantly
       more effort (lists aren't hashable so you can't easily memoize)
       and the more naive solution ran instantly.

--- src/day2.py
+++ src/day2.py
@@ -2,21 +2,24 @@ from filehandling import open_data_file_as_lines
 
 DATA_FILE = "day2in.txt"
 
-DECREASING = 0
-INCREASING = 1
+DECREASING = 1
+INCREASING = 2
 
 
 def main():
     lines = open_data_file_as_lines(DATA_FILE)
     safe_lines = 0
+    tolerable_lines = 0
     for line in lines:
         nums = [int(x) for x in line.split()]
         if is_line_safe(nums):
             safe_lines += 1
+        if is_line_tolerable(nums):
+            tolerable_lines += 1
     print(safe_lines)
+    print(tolerable_lines)
 
-
-
+# Part 1
 def is_line_safe(line):
     if len(line) <= 1:
         return True
@@ -31,10 +34,21 @@ def is_line_safe(line):
         last = num
     return True
 
+# Part 2
+def is_line_tolerable(line):
+    if is_line_safe(line):
+        return True
+    for idx, _ in enumerate(line):
+        if is_line_safe(line[:idx] + line[idx+1:]):
+            return True
+    return False
+
 def what_step_direction(step1, step2):
     return INCREASING if (step1 < step2) else DECREASING
 
-def is_step_safe(step1, step2):
+def is_step_safe(step1, step2, direction = None):
+    if direction is not None and direction != what_step_direction(step1, step2):
+        return False
     diff = abs(step1-step2)
     return (diff > 0 and diff <= 3)
 
