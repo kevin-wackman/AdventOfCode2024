6ff433afdc331a8c0062df41b8b5a931ed9c35bb
kevin-wackman <kevin.wackman@gmail.com>
2024-12-03 19:23:36 -0600

    Added solution to part 2 of day 3
      I ran into a few silly blocks for a bit - one was that I forgot the +
      on the += of the second sum.  That doesn't even matter though, since
      I realized that the whole program remembered the do/don't so I just
      concatenated everything (could be a problem with a massive program),
      but if it came to that I would just use filestreams.

--- src/day3.py
+++ src/day3.py
@@ -2,21 +2,40 @@ from filehandling import open_data_file_as_lines
 import re
 
 DATA_FILE = "day3in.txt"
+DO = "do()"
+DONT = "don't()"
 
 def main():
     lines = open_data_file_as_lines(DATA_FILE)
+    total = 0
+    wholelines = "".join(lines)
+    for line in lines:
+        total += get_line_sum(line)
+    print(total)
+    print(get_sum_do_dont(wholelines))
+
+def get_line_sum(line):
+    if not line:
+        return 0
     # regex lovingly taken from chat GPT
     pattern = r'mul\((\d{1,3}),(\d{1,3})\)'
+    matches = re.findall(pattern, line)
     total = 0
-    for line in lines:
-        matches = re.findall(pattern, line)
-        for match in matches:
-            total += (int(match[0]) * int(match[1]))
-    print(total)
+    for match in matches:
+        total += (int(match[0]) * int(match[1]))
+    return total
 
-
-
-
-
+def get_sum_do_dont(line):
+    if not line:
+        return 0
+    # Split the line on the don't delimeters - the first part is enabled, and then
+    # recursively run the function after filtering for a do on the other parts
+    split_line = line.split(DONT, 1)
+    sum = get_line_sum(split_line[0])
+    if len(split_line) > 1:
+        new_split = split_line[1].split(DO, 1)
+        if len(new_split) > 1:
+            sum += get_sum_do_dont(new_split[1])
+    return sum
 
 main()
