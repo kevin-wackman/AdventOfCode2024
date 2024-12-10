bdb240336345c3a11b1b982e578bb7b56c216430
kevin-wackman <kevin.wackman@gmail.com>
2024-12-10 01:25:15 -0600

    Added part 2 solution to day 5
      Did it recursively again, it seemed easiest to just partition and
      sort each partition.

--- src/day5.py
+++ src/day5.py
@@ -5,10 +5,33 @@ def main():
     lines = open_data_file_as_lines(DATA_FILE)
     (rules, updates) = parse_lines_into_rules_updates(lines)
     part_1_sum = 0
+    part_2_sum = 0
     for update in updates:
-        part_1_sum += score_line(rules, update)
+        n = score_line(rules, update)
+        if n > 0:
+            part_1_sum += n
+        else:
+            part_2_sum += score_incorrect_line(rules, update)
     print(part_1_sum)
+    print(part_2_sum)
 
+# This works on the assumption that the line is incorrect.
+def score_incorrect_line(rules, update):
+    sorted_list = partition_list(rules, update)
+    return sorted_list[(len(sorted_list) - 1)//2]
+
+def partition_list(rules, update):
+    if len(update) <= 1:
+        return update
+    base = update[0]
+    prereqs = []
+    postreqs = []
+    for val in update[1:]:
+        if val in rules[base]:
+            prereqs.append(val)
+        else:
+            postreqs.append(val)
+    return partition_list(rules, prereqs) + [base] + partition_list(rules, postreqs)
 
 def score_line(rules, update):
     if not is_valid_line(rules, update):
@@ -22,8 +45,6 @@ def is_valid_line(rules, update):
         if n in rules[update[0]]:
             return False
     return is_valid_line(rules, update[1:])
-
-        
 
 def parse_lines_into_rules_updates(lines):
     in_rules = True
