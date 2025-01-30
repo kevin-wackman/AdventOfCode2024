a3652d139782a03ec0b0afe46874900b55622b43
kevin-wackman <kevin.wackman@gmail.com>
2025-01-30 14:20:18 -0600

    Add solution to part 2 of day 9
      This solution is a bit naive and takes a few seconds to run, but it
      works and feels pretty clean.  Definitely room for improvement.

--- src/day9.py
+++ src/day9.py
@@ -12,6 +12,9 @@ def get_block_value(block):
 def get_block_size(block):
     return block[1]
 
+def is_free_block(block):
+    return get_block_value(block) == 0
+
 def process_file(line):
     file_blocks = deque()
     free_blocks = deque()
@@ -48,16 +51,56 @@ def process_file(line):
 def get_checksum(blocks):
     checksum_list = [get_block_value(block) for block in blocks for _ in range(get_block_size(block))]
     checksum = reduce(lambda total, n: total + n[0] * n[1], enumerate(checksum_list), 0)
-    print(checksum)
+    return checksum
+
+def process_file_part_2(line):
+    blocks = []
+    for i,size in enumerate(line):
+        if i % 2: # if it's odd, it's free space
+            blocks.append((0, int(size)))
+        else:
+            blocks.append((int(i/2),int(size)))
+    # Using 0 to identify open blocks simplifies a lot of things
+    # However, the first element is also 0.  Popping it from the list
+    # and storing it to psuh back on will save lots of time
+    first_block_element = blocks[0]
+    blocks = blocks[1:]
+    back_pointer = len(blocks) - 1
+    while back_pointer > 0:
+        current_block = blocks[back_pointer]
+        if is_free_block(current_block):
+            back_pointer -= 1
+            continue
+        first_open_block_pointer = get_first_open_block(blocks[:back_pointer], get_block_size(current_block))
+        if first_open_block_pointer < 0:
+            back_pointer -= 1
+            continue
+        # Replace old block
+        blocks[back_pointer] = (0, get_block_size(current_block))
+        free_block_size = get_block_size(blocks[first_open_block_pointer])
+        blocks[first_open_block_pointer] = current_block
+        back_pointer -= 1
+        if get_block_size(current_block) < free_block_size:
+            blocks.insert(first_open_block_pointer + 1, (0,free_block_size - get_block_size(current_block)))
+            back_pointer += 1
+    blocks.insert(0,first_block_element)
+    return blocks
 
 
 
+def get_first_open_block(blocks, size):
+    for i, block in enumerate(blocks):
+        if (is_free_block(block)) and (get_block_size(block) >= size):
+            return i
+    return -1
 
 def main():
     line = open_data_file_as_lines(DATA_FILE)[0].strip()
-    #line = "2333133121414131402"
+    # line = "2333133121414131402"
     defragmented_blocks = process_file(line)
-    get_checksum(defragmented_blocks)
+    better_fragmented_blocks = process_file_part_2(line)
+    print(get_checksum(defragmented_blocks))
+    print(get_checksum(better_fragmented_blocks))
 
 
 main()
