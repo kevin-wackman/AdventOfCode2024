f69214d5d2eb1c8da9498dc90df20ce6d13a2c39
kevin-wackman <kevin.wackman@gmail.com>
2025-01-31 16:12:50 -0600

    Add solution to day 11 part 2
      I added the caching I meant to add in the first place and tried
      brute forcing, but that took way too long.  However, since the order
      of stones doesn't actually matter, I can just track how many of each
      stone and just calculate once per stone.  This also prevents an
      unwieldly list, and just lets me clculate much faster (under 1s)

--- src/day11.py
+++ src/day11.py
@@ -1,11 +1,13 @@
 from filehandling import open_data_file_as_lines
 from functools import cache
-import math
+from collections import defaultdict
+
 SAMPLE_INPUT = "125 17"
 
 
 DATA_FILE = "day11in.txt"
 
+@cache
 def process_stone(stone):
     if stone == 0:
         return [1]
@@ -16,9 +18,11 @@ def process_stone(stone):
     return [2024 * stone]
 
 def blink(row):
-    new_row = []
-    for stone in row:
-        new_row.extend(process_stone(stone))
+    new_row = defaultdict(int)
+    for stone in row.keys():
+        new_stones = process_stone(stone)
+        for new_stone in new_stones:
+            new_row[new_stone] += row[stone]
     return new_row
 
 def main():
@@ -26,8 +30,15 @@ def main():
     line = lines[0].strip()
     # line = SAMPLE_INPUT
     int_line = [int(stone) for stone in line.split(' ')]
-    for _ in range(25):
-        int_line = blink(int_line)
-    print(len(int_line))
+    dict_line = defaultdict(int)
+    for stone in int_line:
+        dict_line[stone] += 1
+    for n in range(75):
+        # print("Processing line {}".format(n))
+        dict_line = blink(dict_line)
+    total_stones = 0
+    for n in dict_line.values():
+        total_stones += n
+    print(total_stones)
 
 main()
