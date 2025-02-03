26606c7ff8a34c7999e0aff31bb9cdba4277a8f4
kevin-wackman <kevin.wackman@gmail.com>
2025-02-03 14:30:41 -0600

    Add solution to day 12 part 2

--- src/day12.py
+++ src/day12.py
@@ -1,8 +1,10 @@
 from filehandling import open_data_file_as_lines
 from collections import deque
-from localutils import get_neighbors, is_in_bounds, get_position
+from localutils import is_in_bounds, get_position, Vector, get_neighbors_with_direction, rotate_clockwise, apply_vector
 
 DATA_FILE = "day12in.txt"
+PART_1 = 0
+PART_2 = 1
 
 SAMPLE_INPUT = [
     "RRRRIICCFF",
@@ -17,43 +19,56 @@ SAMPLE_INPUT = [
     "MMMISSJEEE",
 ]
 
-def find_region_stats(board, explored, start):
+def find_region_stats(board, explored, start, part):
     members = deque()
     members.append(start)
     explored.add(start)
     symbol = get_position(board, start)
     area = 1
     perimeter = 0
+    sides = 0
     while members:
         current = members.popleft()
-        for neighbor in get_neighbors(current):
-            # Check if the cell is of the same type
-            if not is_in_bounds(board, neighbor) or (not (get_position(board, neighbor) == symbol)):
+        for (vector, neighbor) in get_neighbors_with_direction(current):                
+            if (not is_in_bounds(board, neighbor)) or (not (get_position(board, neighbor) == symbol)):
                 perimeter += 1
+                clockwise_direction = rotate_clockwise(vector)
+                if is_symbol(board, apply_vector(neighbor, clockwise_direction), symbol) or \
+                        not is_symbol(board, apply_vector(current, clockwise_direction), symbol):
+                    sides += 1
                 continue
             if neighbor in explored:
                 continue
             members.append(neighbor)
             explored.add(neighbor)
             area += 1
-    return (area, perimeter)
+    if part == PART_1:
+        return (area, perimeter)
+    return (area, sides)
 
-def process_board_part_1(board):
+def is_symbol(board, coords, symbol):
+    if not is_in_bounds(board, coords):
+        return False
+    return symbol == get_position(board, coords)
+
+
+def process_board(board, part):
     explored = set()
     total_price = 0
     for x, row in enumerate(board):
         for y, _ in enumerate(row):
             if (x,y) not in explored:
-                (area, perimeter) = find_region_stats(board, explored, (x,y))
+                (area, perimeter_or_sides) = find_region_stats(board, explored, (x,y), part)
                 # print("A region of {} plants with price {} * {} = {}".format(
                 #     get_position(board, (x,y)), area, perimeter, area*perimeter))
-                total_price += area * perimeter
+                total_price += area * perimeter_or_sides
     return total_price
 
 def main():
     lines = open_data_file_as_lines(DATA_FILE)
     board = [line.strip() for line in lines]
     # board = SAMPLE_INPUT
-    print(process_board_part_1(board))
+    print(process_board(board, PART_1))
+    print(process_board(board, PART_2))
 
 main()
--- src/localutils.py
+++ src/localutils.py
@@ -7,19 +7,37 @@ class Vector(Enum):
     DOWN = (1,0)
     LEFT = (0,-1)
 
+def rotate_clockwise(vec):
+    match vec:
+        case Vector.UP:
+            return Vector.RIGHT
+        case Vector.RIGHT:
+            return Vector.DOWN
+        case Vector.DOWN:
+            return Vector.LEFT
+        case Vector.LEFT:
+            return Vector.UP
+    raise ValueError("Invalid vector {} found".format(vec))
+
 def apply_to_tuples(fn, t1, t2):
     return (fn(t1[0], t2[0]), fn(t1[1], t2[1]))
 
 def add_tuples(t1, t2):
     return apply_to_tuples(add, t1, t2)
 
+def apply_vector(coords, vec):
+    return add_tuples(coords, vec.value)
+
 def string_to_digits(str):
     return [int(char) for char in str.strip()]
 
 def get_neighbors(coords):
+    return [neighbor for (_, neighbor) in get_neighbors_with_direction(coords)]
+
+def get_neighbors_with_direction(coords):
     neighbors = []
     for vector in Vector:
-        neighbors.append(add_tuples(coords, vector.value))
+        neighbors.append((vector, add_tuples(coords, vector.value)))
     return neighbors
 
 def is_in_bounds(map, coords):
